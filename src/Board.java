0434c5fe299717d8d79bea621940df60b38fc909
Kai <keh2214@columbia.edu>
2023-06-14 23:41:53 -0400

    Changes

--- src/Board.java
+++ src/Board.java
@@ -4,11 +4,38 @@ public class Board {
     char[][] letterGrid;
     int size;
     public Board(int size) {
-        this.size = size;
-        letterGrid = new char[size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                letterGrid[i][j] = (char) (((int) (Math.random() * 26)) + 65);
+        if (size == 4) {
+            letterGrid = new char[size][size];
+            ArrayList<ArrayList<Character>> dice = new ArrayList<>();
+            dice.add(Arrays.asList(new char[] {'R', 'I', 'F', 'O', 'B', 'X'}));
+            dice.add(Arrays.asList(new char[] {'E', 'I', 'F', 'H', 'E', 'Y'}));
+            dice.add(Arrays.asList(new char[] {'D', 'E', 'N', 'O', 'W', 'S'}));
+            dice.add(Arrays.asList(new char[] {'D', 'N', 'K', 'O', 'T', 'U'}));
+            dice.add(Arrays.asList(new char[] {'H', 'M', 'S', 'O', 'R', 'A'}));
+            dice.add(Arrays.asList(new char[] {'L', 'U', 'P', 'E', 'T', 'S'}));
+            dice.add(Arrays.asList(new char[] {'A', 'I', 'C', 'O', 'T', 'A'}));
+            dice.add(Arrays.asList(new char[] {'Y', 'L', 'G', 'K', 'U', 'E'}));
+            
+
+            Collections.shuffle(dice);
+            //initialize the list of dices in a 2d ArrayList, then call collections.shuffle on the 2d
+            //iterate from 1 to 16 to assign a dice to each square on the board, as you assign
+            //pick a random character from the list of characters on the dice
+            int die = 0;
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    letterGrid[i][j] = dice.get(die++).get((int) Math.random()*6);
+                }
+            }
+        }
+        else if (size == 5) {
+            //same thing as for size == 4
+
+            letterGrid = new char[size][size];
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    letterGrid[i][j] = (char) (((int) (Math.random() * 26)) + 65);
+                }
             }
         }
     }
@@ -56,3 +83,5 @@ public class Board {
 //check if the word is available on the board
 //based on the result of the previous two checks, print something on the screen, either an error or score
 //verify the size is between 3 and 6 on the front end side (or in game class perhaps)
+//when the size is 4, q will be its own character, but will display on front end and function as "Qu" 
+//when forming words
