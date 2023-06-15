5d3a3d093e9ac1bc1bf1bf3ddc7fcab06c9c578c
Kai <keh2214@columbia.edu>
2023-06-14 23:09:14 -0400

    Board is finished

--- src/Board.java
+++ src/Board.java
@@ -1,13 +1,30 @@
 public class Board {
     char[][] letterGrid;
+    int size;
     public Board(int size) {
+        this.size = size;
         letterGrid = new char[size][size];
         for (int i = 0; i < size; i++) {
-            for (int j = 0; i < size; j++) {
+            for (int j = 0; j < size; j++) {
                 letterGrid[i][j] = (char) (((int) (Math.random() * 26)) + 65);
             }
         }
     }
+
+    public void printBoard() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(letterGrid[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Board board = new Board(5);
+        board.printBoard();
+    }
+
 }
 
 //different scoring options, scoring class or variable (stored as string or boolean maybe)
